9df97da1edd8c37e028f53f85b63784efd0e425e
Baki <danfael175@gmail.com>
2025-03-16 10:03:17 +0300

    Time: 1 ms (48.21%) | Memory: 2.5 MB (49.43%) - LeetSync

--- 1-two-sum/two-sum.rs
+++ 1-two-sum/two-sum.rs
@@ -5,16 +5,10 @@ impl Solution {
         let mut map = HashMap::new();
 
         for (i, num) in nums.iter().enumerate() {
-            if !map.contains_key(num) {
-                map.insert(num, i);
-            }
-        }
-
-        for (i, num) in nums.iter().enumerate() {
             if let Some(j) = map.get(&(target - num)) {
-                if (*j != i) {
-                    return vec![i as i32, *j as i32];
-                }
+                return vec![i as i32, *j as i32];
+            } else {
+                map.insert(num, i);
             }
         }
 
