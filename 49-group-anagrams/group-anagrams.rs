474ae0fe9e3c6510c72ac2a3f7bc4fbd00f9a048
Baki <danfael175@gmail.com>
2025-03-16 10:48:43 +0300

    Time: 0 ms (100.00%) | Memory: 4.8 MB (75.70%) - LeetSync

--- 49-group-anagrams/group-anagrams.rs
+++ 49-group-anagrams/group-anagrams.rs
@@ -2,21 +2,18 @@ use std::collections::HashMap;
 
 impl Solution {
     pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
-        let mut result: HashMap<String, Vec<String>> = HashMap::new();
+        let mut result: HashMap<[u8; 26], Vec<String>> = HashMap::new();
 
         for value in strs {
-            let mut count = vec![0; 26];
-            for c in value.chars() {
-                let key: usize = c.to_ascii_lowercase() as usize - 97;
-                count[key] += 1;
-            }
-            let key: String = count.iter().map(|v| format!("{}#", v.to_string())).collect();
+            let mut freqs = value
+                .bytes()
+                .map(|b| (b - b'a') as usize).fold([0; 26], |mut freqs, bin| {
+                    freqs[bin] += 1;
+                    freqs
+                }
+            );
 
-            if let Some(array) = result.get_mut(&key) {
-                array.push(value);
-            } else {
-                result.insert(key, vec![value]);
-            }
+            result.entry(freqs).or_default().push(value)
         }
 
         result.into_values().collect()
