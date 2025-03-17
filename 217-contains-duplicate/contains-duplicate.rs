abd1c60b5c210b9ec112bbe5942f8d1b487f7eb3
Baki <danfael175@gmail.com>
2025-03-17 07:09:18 +0300

    Time: 7 ms (39.32%) | Memory: 3.9 MB (66.33%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.rs
+++ 217-contains-duplicate/contains-duplicate.rs
@@ -2,7 +2,7 @@ use std::collections::HashSet;
 
 impl Solution {
     pub fn contains_duplicate(nums: Vec<i32>) -> bool {
-        let mut map = HashSet::new();
-        !nums.into_iter().all(|n| map.insert(n))
+        let mut set = HashSet::new();
+        !nums.into_iter().all(|num| set.insert(num))
     }
 }
