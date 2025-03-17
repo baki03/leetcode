c7ceb4c808de32c6d24649e254abba099d16136f
Baki <danfael175@gmail.com>
2025-03-17 07:46:20 +0300

    Time: 4 ms (67.12%) | Memory: 4.7 MB (75.51%) - LeetSync

--- 49-group-anagrams/group-anagrams.rs
+++ 49-group-anagrams/group-anagrams.rs
@@ -2,20 +2,21 @@ use std::collections::HashMap;
 
 impl Solution {
     pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
-        let mut result: HashMap<[u8; 26], Vec<String>> = HashMap::new();
+        let mut map: HashMap<[u8; 26], Vec<String>> = HashMap::new();
 
-        for value in strs {
-            let mut freqs = value
-                .bytes()
-                .map(|b| (b - b'a') as usize).fold([0; 26], |mut freqs, bin| {
+        for s in strs {
+            let mut freqs = s.bytes()
+                .map(|c| (c - b'a') as usize)
+                .fold([0; 26], |mut freqs, bin| {
                     freqs[bin] += 1;
                     freqs
-                }
-            );
+                });
 
-            result.entry(freqs).or_default().push(value)
+            map.entry(freqs)
+                .or_default()
+                .push(s);
         }
 
-        result.into_values().collect()
+        map.into_values().collect()
     }
 }
