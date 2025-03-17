1ef1350f86e3749d700d8acf1690255b30d55943
Baki <danfael175@gmail.com>
2025-03-17 07:17:44 +0300

    Time: 0 ms (100.00%) | Memory: 2.7 MB (5.26%) - LeetSync

--- 242-valid-anagram/valid-anagram.rs
+++ 242-valid-anagram/valid-anagram.rs
@@ -3,17 +3,15 @@ impl Solution {
         if s.len() != t.len() {
             return false;
         }
-        
-        let mut map = vec![0; 26];
+
+        let mut count = vec![0; 26];
         let s: Vec<char> = s.chars().map(|c| c).collect();
         let t: Vec<char> = t.chars().map(|c| c).collect();
         for i in 0..s.len() {
-            let key_s: usize = s[i].to_ascii_lowercase() as usize - 97;
-            let key_t: usize = t[i].to_ascii_lowercase() as usize - 97;
-            map[key_s] += 1;
-            map[key_t] -= 1;
+            count[(s[i] as u8 - b'a') as usize] += 1;
+            count[(t[i] as u8 - b'a') as usize] -= 1;
         }
 
-        map.into_iter().all(|v| if v != 0 { false } else { true })
+        count.into_iter().all(|v| if v != 0 { false } else { true })
     }
 }
