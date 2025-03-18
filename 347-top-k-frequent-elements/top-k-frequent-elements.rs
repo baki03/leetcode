b8008715fd38537aed776e87888ac71cd6ef945b
Baki <danfael175@gmail.com>
2025-03-18 14:32:36 +0300

    Time: 0 ms (100.00%) | Memory: 2.3 MB (100.00%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.rs
+++ 347-top-k-frequent-elements/top-k-frequent-elements.rs
@@ -2,11 +2,10 @@ use std::collections::HashMap;
 
 impl Solution {
     pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
-        let mut map: HashMap<i32, i32> = HashMap::new();
-        nums.into_iter()
-            .for_each(|num| *map.entry(num).or_insert(0) += 1);
+        let mut map = HashMap::new();
+        nums.into_iter().for_each(|n| *map.entry(n).or_default() += 1);
         let mut result: Vec<(i32, i32)> = map.into_iter().collect();
         result.sort_by(|a, b| b.1.cmp(&a.1));
-        result.iter().take(k as usize).map(|x| x.0).collect()
+        result.into_iter().take(k as usize).map(|v| v.0).collect()
     }
 }
