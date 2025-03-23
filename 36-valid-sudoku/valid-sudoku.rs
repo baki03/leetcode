1fda608401baf18baf92a8e1a91a4e3e88cd4cdf
Baki <danfael175@gmail.com>
2025-03-23 10:08:59 +0300

    Time: 0 ms (100.00%) | Memory: 2.2 MB (91.86%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.rs
+++ 36-valid-sudoku/valid-sudoku.rs
@@ -2,25 +2,24 @@ use std::collections::{HashMap, HashSet};
 
 impl Solution {
     pub fn is_valid_sudoku(board: Vec<Vec<char>>) -> bool {
-        let mut cols: HashMap<usize, HashSet<char>> = HashMap::new();
-        let mut rows: HashMap<usize, HashSet<char>> = HashMap::new();
-        let mut square: HashMap<(usize, usize), HashSet<char>> = HashMap::new();
+        let mut cols: [i32; 9] = [0; 9];
+        let mut rows: [i32; 9] = [0; 9];
+        let mut square: [i32; 9] = [0; 9];
 
         for r in 0..9 {
             for c in 0..9 {
                 if board[r][c] == '.' {
                     continue;
                 }
-
-                if rows.entry(r).or_default().contains(&board[r][c]) ||
-                    cols.entry(c).or_default().contains(&board[r][c]) ||
-                    square.entry((r / 3, c / 3)).or_default().contains(&board[r][c]) {
+                let value = board[r][c] as u8 - b'1';
+                if rows[r] & (1 << value) > 0 ||
+                    cols[c] & (1 << value) > 0 ||
+                    square[(r / 3) * 3 + (c / 3)] & (1 << value) > 0 {
                         return false;
                 }
-
-                rows.entry(r).or_default().insert(board[r][c]);
-                cols.entry(c).or_default().insert(board[r][c]);
-                square.entry((r / 3, c / 3)).or_default().insert(board[r][c]);
+                rows[r] |= (1 << value);
+                cols[c] |= (1 << value);
+                square[(r / 3) * 3 + (c / 3)] |= (1 << value);
             }
         }
 
