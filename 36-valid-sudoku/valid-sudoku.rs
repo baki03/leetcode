22ca496ac1b58f1dbbd67a96c3c74f7af23d7e6f
Baki <danfael175@gmail.com>
2025-03-24 09:50:32 +0300

    Time: 0 ms (100.00%) | Memory: 2.2 MB (100.00%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.rs
+++ 36-valid-sudoku/valid-sudoku.rs
@@ -1,7 +1,7 @@
 impl Solution {
     pub fn is_valid_sudoku(board: Vec<Vec<char>>) -> bool {
-        let mut cols: [i32; 9] = [0; 9];
         let mut rows: [i32; 9] = [0; 9];
+        let mut cols: [i32; 9] = [0; 9];
         let mut square: [i32; 9] = [0; 9];
 
         for r in 0..9 {
@@ -9,15 +9,17 @@ impl Solution {
                 if board[r][c] == '.' {
                     continue;
                 }
+
                 let value = board[r][c] as u8 - b'1';
                 if rows[r] & (1 << value) > 0 ||
                     cols[c] & (1 << value) > 0 ||
                     square[(r / 3) * 3 + (c / 3)] & (1 << value) > 0 {
-                        return false;
+                    return false;
                 }
-                rows[r] |= (1 << value);
-                cols[c] |= (1 << value);
-                square[(r / 3) * 3 + (c / 3)] |= (1 << value);
+
+                rows[r] |= 1 << value;
+                cols[c] |= 1 << value;
+                square[(r / 3) * 3 + (c / 3)] |= 1 << value;
             }
         }
 
