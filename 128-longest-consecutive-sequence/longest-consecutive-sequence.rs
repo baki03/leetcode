14a6217b673af14e28e77c625915e2876eed2378
Baki <danfael175@gmail.com>
2025-03-25 13:04:32 +0300

    Time: 29 ms (20.00%) | Memory: 4.8 MB (15.88%) - LeetSync

---
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.rs
@@ -0,0 +1,22 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn longest_consecutive(nums: Vec<i32>) -> i32 {
+        let mut map = HashMap::new();
+        let mut longest = 0;
+
+        for num in nums {
+            if !map.contains_key(&num) {
+                let before = *map.get(&(num - 1)).unwrap_or(&0);
+                let after = *map.get(&(num + 1)).unwrap_or(&0);
+                let value = before + after + 1;
+                map.insert(num, value);
+                map.insert(num - before, value);
+                map.insert(num + after, value);
+                longest = longest.max(value);
+            }
+        }
+
+        longest
+    }
+}
