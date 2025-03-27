9951c82e9ec725b5fcd05b1bd7a638042e2d6121
Baki <danfael175@gmail.com>
2025-03-27 16:23:16 +0300

    Time: 0 ms (100.00%) | Memory: 2.4 MB (34.70%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.rs
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.rs
@@ -2,7 +2,7 @@ impl Solution {
     pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
         let mut k = 1;
         for i in 1..nums.len() {
-            if nums[i] != nums[i - 1] {
+            if nums[i - 1] != nums[i] {
                 nums[k] = nums[i];
                 k += 1;
             }
