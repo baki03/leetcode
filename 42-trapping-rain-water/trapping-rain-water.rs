e53a6b2138bda8524aae61e4f5426df7e158d7ba
Baki <danfael175@gmail.com>
2025-03-31 09:14:36 +0300

    Time: 0 ms (100.00%) | Memory: 2.2 MB (93.91%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.rs
+++ 42-trapping-rain-water/trapping-rain-water.rs
@@ -2,22 +2,21 @@ impl Solution {
     pub fn trap(height: Vec<i32>) -> i32 {
         let mut left = 0;
         let mut right = height.len() - 1;
+        let mut max_left = height[left];
+        let mut max_right = height[right];
+        let mut count = 0;
 
-        let mut left_max = height[left];
-        let mut right_max = height[right];
-
-        let mut result = 0;
         while left < right {
-            if left_max < right_max {
+            if max_left < max_right {
                 left += 1;
-                left_max = left_max.max(height[left]);
-                result += left_max - height[left];
+                max_left = max_left.max(height[left]);
+                count += max_left - height[left];
             } else {
                 right -= 1;
-                right_max = right_max.max(height[right]);
-                result += right_max - height[right];
+                max_right = max_right.max(height[right]);
+                count += max_right - height[right];
             }
         }
-        result
+        count
     }
 }
