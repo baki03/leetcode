278510955ab3e15a8e17d8b17a9544708b9034cb
Baki <danfael175@gmail.com>
2025-04-02 14:06:06 +0300

    Time: 0 ms (100.00%) | Memory: 2.8 MB (79.75%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.rs
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.rs
@@ -1,33 +1,28 @@
+use std::ops::{Add, Sub, Mul, Div};
+
 impl Solution {
+    fn get_operator(token: &str) -> Option<fn(i32, i32) -> i32> {
+        match token {
+            "+" => Some(i32::add),
+            "-" => Some(i32::sub),
+            "*" => Some(i32::mul),
+            "/" => Some(i32::div),
+            _ => None
+        }
+    }
+
     pub fn eval_rpn(tokens: Vec<String>) -> i32 {
         let mut stack = Vec::new();
 
         for token in tokens {
-            match token.as_str() {
-                "+" => {
-                    let right = stack.pop().unwrap();
-                    let left = stack.pop().unwrap();
-                    stack.push(left + right);
-                },
-                "-" => {
-                    let right = stack.pop().unwrap();
-                    let left = stack.pop().unwrap();
-                    stack.push(left - right);
-                },
-                "*" => {
-                    let right = stack.pop().unwrap();
-                    let left = stack.pop().unwrap();
-                    stack.push(left * right);
-                },
-                "/" => {
-                    let right = stack.pop().unwrap();
-                    let left = stack.pop().unwrap();
-                    stack.push(left / right);
-                },
-                _ => stack.push(token.parse::<i32>().unwrap())
+            if let Some(op) = Solution::get_operator(token.as_str()) {
+                let right = stack.pop().unwrap();
+                let left = stack.pop().unwrap();
+                stack.push(op(left, right));
+            } else {
+                stack.push(token.parse::<i32>().unwrap());
             }
         }
-
         stack.pop().unwrap()
     }
 }
