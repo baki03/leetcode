d9db46650cf4c215797aa2f675918780e20de6b0
Baki <danfael175@gmail.com>
2025-04-04 15:25:50 +0300

    Time: 0 ms (100.00%) | Memory: 2.4 MB (31.94%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.rs
@@ -0,0 +1,35 @@
+impl Solution {
+    fn backtrack(pair: (i32, i32), n: i32, res: &mut Vec<String>, stack: String) {
+        let (open_n, closed_n) = pair;
+
+        if open_n == closed_n && open_n == n {
+            res.push(stack);
+            return;
+        }
+
+        if open_n < n {
+            Solution::backtrack(
+                (open_n + 1, closed_n),
+                n,
+                res,
+                format!("{}(", stack)
+            );
+        }
+
+        if closed_n < open_n {
+            Solution::backtrack(
+                (open_n, closed_n + 1),
+                n,
+                res,
+                format!("{})", stack)
+            );
+        }
+    }
+
+    pub fn generate_parenthesis(n: i32) -> Vec<String> {
+        let mut res = Vec::new();
+        let stack = String::new();
+        Solution::backtrack((0, 0), n, &mut res, stack);
+        res
+    }
+}
