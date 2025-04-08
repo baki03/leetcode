d8cb1bebe201ecc57ff6f5dfff0539f0cbbd2297
Baki <danfael175@gmail.com>
2025-04-08 16:21:16 +0300

    Time: 11 ms (12.50%) | Memory: 5.3 MB (26.25%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.rs
@@ -0,0 +1,21 @@
+impl Solution {
+    pub fn daily_temperatures(temperatures: Vec<i32>) -> Vec<i32> {
+        let mut stack: Vec<(usize, i32)> = Vec::new();
+        let mut res = vec![0; temperatures.len()];
+
+        for i in 0..temperatures.len() {
+            let current_temp = temperatures[i];
+            while let Some((index, temp)) = stack.last() {
+                if current_temp > *temp {
+                    res[*index] = (i - *index) as i32;
+                    stack.pop();
+                } else {
+                    break;
+                }
+            }
+            stack.push((i, current_temp));
+        }
+
+        res
+    }
+}
